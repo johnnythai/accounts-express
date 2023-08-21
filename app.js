dfcfa141556d4e18a582077731967c4ba12e628d
Johnny Thai <johnnythai@Johnnys-Air.attlocal.net>
2023-08-20 23:30:24 -0700

    app

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = 4001;
 
+require('dotenv').config();
+const CONSUMER_KEY = process.env.CONSUMER_KEY
+const CONSUMER_SECRET = process.env.CONSUMER_SECRET
+
+const jwt = require('jsonwebtoken');
+
 // Define a basic route
 app.get('/', (req, res) => {
-  res.send('Hello, API!');
+  res.send('accounts-express app for FIS HORIZON API');
+});
+
+// Access token
+app.get('/access-token', (req, res) => {
+	fetchAccessToken()
+	res.send('token')
 });
 
 // Start the server
@@ -12,3 +24,24 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+const fetchAccessToken = () => {
+	const apiUrl = 'https://api-gw-uat.fisglobal.com/token';
+
+	const base64Credentials = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString('base64');
+	const requestBody = 'grant_type=client_credentials';
+
+	console.log('base64Credentials:', base64Credentials)
+	console.log('requestBody:', requestBody)
+
+	fetch(apiUrl, {
+		method: 'POST',
+		headers: {
+			'Authorization': 'Basic ${base64Credentials}',
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: requestBody,
+	})
+	.then(res => res.json())
+	.then(data => console.log(data))
+	.catch(error => console.error('Error:', error))
+}
