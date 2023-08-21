3914588a1b35ae23e764353f9e32fde3ccda7ec9
Johnny Thai <johnnythai@Johnnys-Air.attlocal.net>
2023-08-21 00:20:21 -0700

    app

--- app.js
+++ app.js
@@ -10,39 +10,50 @@ const jwt = require('jsonwebtoken');
 
 // Define a basic route
 app.get('/', (req, res) => {
-  res.send('accounts-express app for FIS HORIZON API');
+	res.status(200).send('accounts-express app for FIS HORIZON API')
 });
 
 // Access token
-app.get('/access-token', (req, res) => {
-	const token = fetchAccessToken()
-	console.log(token)
-	res.send('token')
+app.get('/access-token', async (req, res) => {
+	try {
+		const token = await fetchAccessToken();
+		console.log('TOKEN:', token);
+		res.status(200).send(token)
+	} catch (error) {
+		console.error('Access token error:', error);
+		res.status(500).send('Error fetching access token');
+	}
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
 
-const fetchAccessToken = () => {
+const fetchAccessToken = async () => {
 	const apiUrl = 'https://api-gw-uat.fisglobal.com/token';
 
 	const base64Credentials = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString('base64');
 	const requestBody = 'grant_type=client_credentials';
 
-	console.log('base64Credentials:', base64Credentials)
-	console.log('requestBody:', requestBody)
+	try {
+		const response = await fetch(apiUrl, {
+			method: 'POST',
+			headers: {
+				'Authorization': `Basic ${base64Credentials}`,
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: requestBody,
+		});
 
-	fetch(apiUrl, {
-		method: 'POST',
-		headers: {
-			'Authorization': `Basic ${base64Credentials}`,
-			'Content-Type': 'application/x-www-form-urlencoded',
-		},
-		body: requestBody,
-	})
-	.then(res => res.json())
-	.then(data => console.log(data))
-	.catch(error => console.error('Error:', error))
-}
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+
+		const data = await response.json();
+		return data.access_token; // Return the access token
+	} catch (error) {
+		console.error('Fetch access token error:', error);
+		throw error;
+	}
+};
