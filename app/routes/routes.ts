41849d9b29d5968bb3aaf89087a7360d80f674ba
Johnny Thai <johnnythai@Johnnys-MacBook-Air.local>
2023-08-23 10:25:13 -0700

    test

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -4,12 +4,14 @@ const router = express.Router()
 const { fetchFisToken, fetchHorizonToken } = require('../controllers/fetch.ts')
 
 // Home 
-router.get('/', (req, res) => {
+router.get('/', (req: any, res: any) => {
 	res.status(200).send('accounts-express app for FIS HORIZON API');
 });
 
 // Authorization tokens
 router.get('/authorization/:token', async (req, res) => {
+	console.log('req object:', req);
+	console.log('re.params:', req.params);
 	const requestedToken = req.params.token;
 	console.log(requestedToken, ' token requested');
 
