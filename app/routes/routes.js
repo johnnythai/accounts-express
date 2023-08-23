9e06c613941b85f408304452a3fbbb3924686bee
Johnny Thai <johnnythai@Johnnys-Air.attlocal.net>
2023-08-22 18:59:08 -0700

    js

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router()
 
 const { fetchFisToken, fetchHorizonToken } = require('../controllers/fetch')
