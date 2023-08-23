b3b7a0a8844121c4cb6a4ae576fb80e3bfbf3235
Johnny Thai <johnnythai@Johnnys-MacBook-Air.local>
2023-08-23 09:49:55 -0700

    ts

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 
-const { fetchFisToken, fetchHorizonToken } = require('../controllers/fetch')
+const { fetchFisToken, fetchHorizonToken } = require('../controllers/fetch.ts')
 
 // Home 
 router.get('/', (req, res) => {
