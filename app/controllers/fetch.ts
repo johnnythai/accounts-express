a207aae65e0fd72f602e15309d2b0abd55c9a29d
Johnny Thai <johnnythai@Johnnys-MacBook-Air.local>
2023-08-31 13:07:00 -0700

    requesting tokens

--- app/controllers/fetch.ts
+++ app/controllers/fetch.ts
@@ -1,5 +1,3 @@
-const express = require('express');
-
 const { v4: uuidv4 } = require('uuid');
 
 require('dotenv').config();
@@ -7,9 +5,11 @@ const CONSUMER_KEY = process.env.CONSUMER_KEY;
 const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
 const ORGANIZATION_ID = process.env.ORGANIZATION_ID;
 const SOURCE_ID = process.env.SOURCE_ID;
+const FIS_USER_ID = process.env.FIS_USER_ID;
+const FIS_USER_SECRET = process.env.FIS_USER_SECRET;
 
 
-const fetchApi = async (apiUrl: string, options?: {requestBody: {}}) => {
+const fetchApi = async (apiUrl: string, options?: {}) => {
 	try {
 		const response = await fetch(apiUrl, options);
 		return(response);
@@ -24,7 +24,7 @@ const fetchFisToken = async (req: {}) => {
 	const requestBody = 'grant_type=client_credentials';
 
 	const options = {
-		method: 'POST',
+		method: 'PUT',
 		headers: {
 			'Authorization': `Basic ${base64Creds}`,
 			'Content-Type': 'application/x-www-form-urlencoded',
@@ -37,26 +37,29 @@ const fetchFisToken = async (req: {}) => {
 	return(token.access_token);
 };
 
-const fetchHorizonToken = async (req: {fisToken: string, body:{}}) => {
+const fetchHorizonToken = async (req: {fisToken: string, body:{userId: string, userSecret: string}}) => {
 	const apiUrl = 'https://api-gw-uat.fisglobal.com/rest/horizon/authorization/v2/authorization';
-	const fisToken = req.fisToken;
-	const requestBody = req.body
-	
-	const id = uuidv4();
-	console.log('uuid:', id);
+	const fisTemp = 'eyJ4NXQiOiJOemRrWkRjMU9XWmtPVFE0WTJNeE1XUXdOR013WkRjMVkyUXlaakEyWXpjMVl6TTROalZtTlRSaE16SXdZelV3TURVMU9UY3paVFE1T1dFMk9USm1OZyIsImtpZCI6Ik56ZGtaRGMxT1daa09UUTRZMk14TVdRd05HTXdaRGMxWTJReVpqQTJZemMxWXpNNE5qVm1OVFJoTXpJd1l6VXdNRFUxT1RjelpUUTVPV0UyT1RKbU5nX1JTMjU2IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJqb2hubnl0aGFpIiwiYXV0IjoiQVBQTElDQVRJT04iLCJhdWQiOiJMSEdLbndXTGxXSUtlTENNZEJwY0FkSlNSOXNhIiwibmJmIjoxNjkzNTA0NzgzLCJhenAiOiJMSEdLbndXTGxXSUtlTENNZEJwY0FkSlNSOXNhIiwic2NvcGUiOiJkZWZhdWx0IiwiaXNzIjoiaHR0cHM6XC9cLzEwMC43Mi4zNS4yNTo5NDQzXC9vYXV0aDJcL3Rva2VuIiwiZXhwIjoxNjkzNTA4MzgzLCJpYXQiOjE2OTM1MDQ3ODMsImp0aSI6IjdlMDRjZDMwLTUyYzQtNGI1MS04MmQ0LWI1NGQ1YzU2NjQ3ZSJ9.sA9ap2l55g-GeoVIYozi95U1bK_FbLL7I_Q0iU8ro2EAFmreL_IS1mMBTH6XC3MsgHxsAFELj5q-lr_6BsxWbB2QPv_kMjknxLV2poyVQofbHouLzElbdWPRfrXwNTU7186WsNdxaTNLwhhEn6PIOBHWEhaege9MGyTpj1daRK-UmEMtgrwmepyhpL0UgFYWqyAnJEx_qGMIEdJCAOn_JschKJolTYxA9uSzliLuVSFZpf1re5-YPBlnIiCH9DULN3uuY1CFHDEa0NxDrMn4PNgeBe0uDwHZdmjPxmucoo1_ZtjbKPZ7KXwQs_81hmKUiTcguauCBJulcbLNqTuX4w'
 
 	const options = {
-		method: 'POST',
+		method: 'PUT',
 		headers: {
-			'accept': 'application/json',
 			'organization-id': ORGANIZATION_ID,
-			'uuid': id,
+			'uuid': uuidv4(),
 			'source-id': SOURCE_ID,
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${fisToken}`,
+			'Authorization': `Bearer ${fisTemp}`,
+			'Content-Type': 'application/json'
 		},
-		body: requestBody,
+		body: {
+			'userId': FIS_USER_ID,
+		       	'userSecret': FIS_USER_SECRET 
+		},
 	};
+
+	const auth = await fetchApi(apiUrl, options);
+	// const token = await auth.json();
+	console.log('TOKEN_______________________', auth);
+	return(auth);
 };
 
 exports.fetchFisToken = fetchFisToken;
