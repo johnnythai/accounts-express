75bc06095c2737d64c9959b3f09e1637d0044c39
Johnny Thai <jthai04@gmail.com>
2023-09-13 16:11:51 -0700

    typo

--- app/routes/horizonRoutes.ts
+++ app/routes/horizonRoutes.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const horizonRouter = express.Router();
 const cookiesMiddleware = require('universal-cookie-express');
 const { fetchCustomerRelationshipSummary } = require('../controllers/horizonController');
-const { fetchAcountInfo } = require('../controllers/horizonController');
+const { fetchAccountInfo } = require('../controllers/horizonController');
 
 horizonRouter.get('/', (req, res) => {
 	res.status(200).send('horizon endpoint');
@@ -11,7 +11,7 @@ horizonRouter.get('/', (req, res) => {
 // Retreive account info 
 horizonRouter.get('/accounts/:applicationCode/:accountNumber', (req, res) => {
 	console.log('Fetching account info');
-	fetchAcountInfo(req, res);
+	fetchAccountInfo(req, res);
 });
 
 // Retrieves list of application/accounts for a customer
